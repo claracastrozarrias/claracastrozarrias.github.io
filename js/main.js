fbd01ad03811e887b589e297c9190c695ab8b19d
claracastrozarrias <claracastromp@gmail.com>
2025-10-15 15:59:32 +0200

    Avances Portofolio

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 (function ($) {
     "use strict";
 
+    AOS.init();
+
     // Navbar on scrolling
     $(window).scroll(function () {
         if ($(this).scrollTop() > 200) {
@@ -30,11 +32,10 @@
 
     // Typed Initiate
     if ($('.typed-text-output').length == 1) {
-        var typed_strings = $('.typed-text').text();
-        var typed = new Typed('.typed-text-output', {
-            strings: typed_strings.split(', '),
-            typeSpeed: 100,
-            backSpeed: 20,
+        //var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {//
+            strings: ['Marketing and Photo Specialist','Estrategia, Imagen y Creatividad'],
+            typeSpeed: 60,
             smartBackspace: false,
             loop: true
         });
